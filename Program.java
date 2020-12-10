a25eddf0b5623df79ff55b16eab7f3b895719ca3
preeti biradar <biradarpreeti25@gmail.com>
2020-12-10 22:48:05 +0530

    second commit

--- Program.java
+++ Program.java
@@ -2,6 +2,10 @@ class Program
 {
     public static void main(String[] args) {
         System.out.println("hellow world");
+        System.out.println("hellow world");
+        System.out.println("hellow world");
+
+
 
     }
 }
