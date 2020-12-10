9c3f7df891b45efd5b70b33f02dc6536eedec20a
preeti biradar <biradarpreeti25@gmail.com>
2020-12-10 22:58:11 +0530

    second project

---
+++ Program1.java
@@ -0,0 +1,15 @@
+package project1;
+
+public class Program1 {
+
+    public static void main(String[] args) {
+        System.out.println("hellow world !!!!!!");
+        System.out.println("hellow world !!!!!!!");
+        System.out.println("hellow world !!!!!!!!");
+
+        
+
+
+    }
+    
+}
